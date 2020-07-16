a90f2896ae606a118636cdaae3dfe6d3cb3b8130
Daniel Chin <daniel_chin@yahoo.com>
2020-07-16 18:14:47 +0800

    better saveFrame

--- saveFrame.m
+++ saveFrame.m
@@ -1,2 +1,7 @@
 % saveas(gcf, sprintf('./output/%d.png', render_i));
+if render_i > 0
+  saveas(gcf, sprintf('E:/IBM_Space/output/%d.png', render_i));
+else
+  pause(1);
+end
 render_i = render_i + 1;
