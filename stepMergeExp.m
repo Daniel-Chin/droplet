5b70658d1a718aa6f4903f4c2a409283ef4114be
Daniel Chin <daniel_chin@yahoo.com>
2020-08-12 08:35:05 +0800

    notes

--- stepMergeExp.m
+++ stepMergeExp.m
@@ -7,16 +7,16 @@ XX=X+(dt/2)*vec_interp(u, X, Nb); % Euler step to midpoint
 XX3 = X3 + (dt/2) * vec_interp(u, X3, Nb3); % Euler step to midpoint
 XX4 = X4 + (dt/2) * vec_interp(u, X4, Nb4); % Euler step to midpoint
 ff=vec_spread(ForceSurface(XX, links, dtheta, K, wall_links, Nb), XX, dtheta, Nb); % Force at midpoint
-% YY4 = Y4 + V4 * dt;
-% force4 = forcePib(YY4 - XX4, pIB_STIFF);
-% ff4 = vec_spread( ...
-%   force4, ...
-%   XX4, dtheta4, Nb4 ...
-% );
-% V4 = V4 - force4 * dt / MASS_PER_POINT;
-% Y4 = Y4 + V4 * dt;
-% total_ff = ff + ff4;
-total_ff = ff;
+YY4 = Y4 + V4 * dt;
+force4 = forcePib(YY4 - XX4, pIB_STIFF);
+ff4 = vec_spread( ...
+  force4, ...
+  XX4, dtheta4, Nb4 ...
+);
+V4 = V4 - force4 * dt / MASS_PER_POINT;
+Y4 = Y4 + V4 * dt;
+total_ff = ff + ff4;
+% total_ff = ff;
 total_ff = total_ff + total_ff(end:-1:1, :, :) .* MIRROR;
 [u,uu]=fluid(u,total_ff); % Step Fluid Velocity
 
