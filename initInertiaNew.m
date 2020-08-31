372ef49fb4d9b28c4a99834eeef365413e46e65c
Daniel Chin <daniel_chin@yahoo.com>
2020-08-31 22:58:54 +0800

    multi initial droplets

---
+++ initInertiaNew.m
@@ -0,0 +1,26 @@
+% Using pIB to simulate water-air density difference
+
+pIB_STIFF = 1; % change so that max|X−Y| <= h/10
+
+dtheta4 = h / 2;
+estimated_area = 0;
+for k = 1 : n_init_droplets
+  estimated_area = estimated_area + (init_circle_rad(k) / dtheta4) ^ 2 * pi;
+end
+X4 = zeros(floor(estimated_area * .7), 2);
+Nb4 = 0;
+for i = 0 : dtheta4 : L
+  for j = 0 : dtheta4 : L
+    for k = 1 : n_init_droplets
+      if norm([i, j] - init_circle_pos(k, :)) < init_circle_rad(k)
+        Nb4 = Nb4 + 1;
+        X4(Nb4, :) = [i, j];
+        break;
+      end
+    end
+  end
+end
+
+Y4 = X4;
+V4 = zeros(Nb4, 2);
+MASS_PER_POINT = (rho_heavy - rho) * dtheta4 ^ 2;
---
+++ initX_multi_circle.m
@@ -0,0 +1,33 @@
+% Six droplets merging
+n_init_droplets = 6;
+init_circle_rad = ones(n_init_droplets, 1) * L * .06;
+init_circle_pos = zeros(n_init_droplets, 2);
+
+for j = 1 : n_init_droplets
+  theta = 2 * pi / n_init_droplets * j;
+  init_circle_pos(j, :) = [L/4, L/2] + L * .16 * [cos(theta), sin(theta)];
+end
+
+dtheta = h / 2; % IB point spacing
+circle_n_points = ceil(pi * 2 * init_circle_rad / dtheta);
+Nb = sum(circle_n_points);
+
+links = zeros(2, Nb);
+wall_links = zeros(2, 0);  % special points that attach to the wall
+X = zeros(Nb, 2);
+
+offset = 0;
+for j = 1 : n_init_droplets
+  n = circle_n_points(j);
+  % 1 clockwise, 2 counter-clockwise
+  theta = linspace(0, pi*2, n+1)';
+  theta = theta(1:end-1); % prevent points form overlapping
+  X(offset+1 : offset+n, :) = init_circle_pos(j, :) + init_circle_rad(j) * [sin(theta), cos(theta)];
+  for k = 1 : n
+    links(1, offset + k) = offset + k + 1;
+    links(2, offset + k) = offset + k - 1;
+  end
+  links(2, offset + 1) = offset + n;
+  links(1, offset + n) = offset + 1;
+  offset = offset + n;
+end
