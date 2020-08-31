343a48e966bf89b2b0046b19bb0e102679833267
Daniel Chin <daniel_chin@yahoo.com>
2020-08-31 15:06:08 +0800

    debug

--- debug.m
+++ debug.m
@@ -1,11 +1,23 @@
-startid = 140;
+startid = 2;
 
 id = links(2, startid);
 points = [];
 points_i = 0;
+up = 0;
+down = inf;
+out_id = -1;
 while id ~= startid
   points_i = points_i + 1;
   points(points_i, :) = X(id, :);
+  y = X(id, 2);
+  if y < down
+    down = y;
+    out_id = id;
+  end
+  if y > up
+    up = y;
+  end
   id = links(2, id);
 end
 plot(points(:,1),points(:,2),'ko');
+disp(out_id);
