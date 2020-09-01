6b0243430e79faa7125b171c38b2625af3d045e9
Daniel Chin <daniel_chin@yahoo.com>
2020-09-01 11:13:55 +0800

    fix wall linker parameter

--- stepMergeExp.m
+++ stepMergeExp.m
@@ -4,7 +4,7 @@ hold on;
 XX=X+(dt/2)*vec_interp(u, X, Nb); % Euler step to midpoint
 XX3 = X3 + (dt/2) * vec_interp(u, X3, Nb3); % Euler step to midpoint
 XX4 = X4 + (dt/2) * vec_interp(u, X4, Nb4); % Euler step to midpoint
-ff=vec_spread(ForceSurface(XX, links, dtheta, K, wall_links, Nb), XX, dtheta, Nb); % Force at midpoint
+ff=vec_spread(ForceSurface(XX, links, dtheta, K, wall_links, Nb, WALL_LINKER_TO_WALL_STIFF), XX, dtheta, Nb); % Force at midpoint
 YY4 = Y4 + V4 * dt;
 force4 = forcePib(YY4 - XX4, pIB_STIFF);
 ff4 = vec_spread( ...
