70a45e0e3ac37794d2686f4dc9c00d0c5f1e64ca
Daniel Chin <daniel_chin@yahoo.com>
2020-11-04 21:52:22 +0800

    good constants for main exp

--- defineConstants.m
+++ defineConstants.m
@@ -1,21 +1,22 @@
-dt = 0.00002; % Time step, second
+dt = 0.0001; % Time step, second
 N = 96; % Number of grid cells
+% N = 128; % Number of grid cells
 L = 2.0; % Box size, cm
 
-K = 20; % Surface tension coefficient, N*10^-5
+K = 30; % Surface tension coefficient, N*10^-5
 % FAKE_REPEL_K = .00001;
 rho = .1; % air density g/cm2
 rho_heavy = 1; % density g/cm2
-mu = .002; % viscosity g/s. 2D water can be 0.00089
-tmax = 4; % Run until time s
+mu = .01; % viscosity g/s. 2D water can be 0.00089
+tmax = .1; % Run until time s
 big_G = 980; % cm/s2
-NO_SLIP_FORCE = 100;
-FRICTION_ADJUST = 100;
+NO_SLIP_FORCE = 25;
+FRICTION_ADJUST = 30;
 SLIP_LENGTH_UNITS = 6;
 
-WALL_STIFFNESS = 22000;
-WALL_LINKER_TO_WALL_STIFF = 20000;
-pIB_STIFF = 10000; % change so that max|X−Y| <= h/10
+WALL_STIFFNESS = 5000;
+WALL_LINKER_TO_WALL_STIFF = 5000;
+pIB_STIFF = 500; % change so that max|X−Y| <= h/10
 
 VERTICAL_FLOW = 0;
 
