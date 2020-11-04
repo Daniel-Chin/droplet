4af8260fe33d8e8a39a82addfb6f995e244cabca
Daniel Chin <daniel_chin@yahoo.com>
2020-11-04 21:31:13 +0800

    crazy linked list edge case: 1-item loop splicing

--- fillLinksHole.m
+++ fillLinksHole.m
@@ -1,17 +1,18 @@
 if holeToFill == Nb
-
+  % do nothing
 else
-  X(holeToFill, :) = X(Nb, :);
-  links(:, holeToFill) = links(:, Nb);
-  % in case moved a wall point into hole
   if any(links(:, Nb) == 1) && any(wall_links(1, :) == Nb)
+    % move a wall point into hole
     wall_links_l_i = wall_links(1, :) == Nb;
     wall_links(1, wall_links_l_i) = holeToFill;
     direction = wall_links(2, wall_links_l_i);
-    links(3 - direction, links(direction, holeToFill)) = holeToFill;
+    links(3 - direction, links(direction, Nb)) = holeToFill;
   else
-    links(1, links(2, holeToFill)) = holeToFill;
-    links(2, links(1, holeToFill)) = holeToFill;
+    links_1_Nb = links(1, Nb);  % Save in case it's a 1-item loop
+    links(1, links(2, Nb)) = holeToFill;
+    links(2, links_1_Nb) = holeToFill;
   end
+  X(holeToFill, :) = X(Nb, :);
+  links(:, holeToFill) = links(:, Nb);
 end
 Nb = Nb - 1;
