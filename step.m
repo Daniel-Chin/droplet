a7231592b3904785beabf7b3c054d11dde7ceb5a
Daniel Chin <daniel_chin@yahoo.com>
2020-11-07 19:41:39 +0800

    double resample each step

--- step.m
+++ step.m
@@ -52,6 +52,7 @@ X2 = X2 + dt * vec_interp(uu, X2, Nb2); % cannot be XX2 because forceWall alters
 X3 = X3 + dt * vec_interp(uu, XX3, Nb3);
 X4 = X4 + dt * vec_interp(uu, XX4, Nb4);
 surfaceResample();
+surfaceResample();  % Doing only once may leave 1-node interface
 surface_velocity = vec_interp(uu, X, Nb);
 surfaceSplice();
 warpIndicators;
