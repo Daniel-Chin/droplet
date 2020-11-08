fbe0a2cc2dd88fb5f8f609ceefef59046f8cef67
Daniel Chin <daniel_chin@yahoo.com>
2020-11-08 12:22:08 +0800

    shrink problem

--- jeez2.m
+++ jeez2.m
@@ -1,11 +1,11 @@
 hold off
 load e1
-plot([1:clock] * dt, area_his, 'b-', 'linewidth', 1);
-% plot([1:clock] * dt, resample_energy_offset_array, 'b-', 'linewidth', 1);
+plot([1:clock] * dt, area_his(1:clock), 'b-', 'linewidth', 1);
+% plot([1:clock] * dt, resample_energy_offset_array(1:clock), 'b-', 'linewidth', 1);
 hold on
 load e2
-plot([1:clock] * dt, area_his, 'r:', 'linewidth', 1);
-% plot([1:clock] * dt, resample_energy_offset_array, 'r:', 'linewidth', 1);
-% axis([0 .1 0 .28]);
+plot([1:clock] * dt, area_his(1:clock), 'r:', 'linewidth', 1);
+% plot([1:clock] * dt, resample_energy_offset_array(1:clock), 'r:', 'linewidth', 1);
+axis([0 .45 0 .14]);
 h = legend('N = 64', 'N = 128', 'N = 192');
 set(h,'FontSize',12);
