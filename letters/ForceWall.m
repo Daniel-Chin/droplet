c69b83d7cb73c67564d9058c82ce69c15d17f469
Daniel Chin <daniel_chin@yahoo.com>
2021-01-01 10:14:53 +0800

    slip with arbitrary membrane

--- letters/ForceWall.m
+++ letters/ForceWall.m
@@ -1,35 +1,29 @@
 function [F, new_X2]=ForceWall(XX2, WALL_STIFFNESS, u, X, Nb, Nb2, NO_SLIP_FORCE, X2, SLIP_LENGTH_COEF, h, FRICTION_ADJUST, wall_links, links, SLIP_LENGTH, kp, km, dtheta2)
 % penalty
-F = WALL_STIFFNESS * (XX2(kp,:) + XX2(km,:) - 2*XX2) / dtheta2;
+F = WALL_STIFFNESS * (XX2(kp, :) + XX2(km, :) - 2*XX2) / dtheta2;
 F(1, :) = 0;
 F(end, :) = 0;
+
+tmpF = F;
 new_X2 = X2;
-return;
-point_velocity = vec_interp(u, X, Nb);
-for k = wall_links
-  id = k(1);
-  direction = k(2);
-  contact_line_vertical_v = point_velocity(id, 2);
-  next2 = links(direction, links(direction, id));
-  tangent_vector = X(next2, :) - X(id, :);
-  contact_angle = pi/2 + atan(tangent_vector(2) / tangent_vector(1));
-  static_limit = friction(contact_angle, contact_line_vertical_v, direction, NO_SLIP_FORCE, FRICTION_ADJUST) * SLIP_LENGTH_COEF;
-  % static_limit
-  entered_slip_region = false;
-  for j=1:Nb2
-    if abs(XX2(j, 2) - X(id, 2)) <= SLIP_LENGTH / 2
-      entered_slip_region = true;
-      intention = F(j, 2);
-      if intention / static_limit > 1
-        % fprintf('Slip! y=%.1f, level=%.2f \n', X2(j, 2), intention / the_limit);
-        plot(X2(j, 1), X2(j, 2), 'ko');
-        F(j, 2) = static_limit;
-        new_X2(j, 2) = PERFECT_WALL(j, 2) - static_limit / WALL_STIFFNESS;
-      end    
-    else
-      if entered_slip_region
-        break;
-      end
-    end
+slip_iter = 0;
+while 1
+  tangent_vector = new_X2(kp, :) - new_X2(km, :);
+  unit_tangent_vector = tangent_vector ./ vecnorm(tangent_vector')';
+  tangent_stress = dot(tmpF, unit_tangent_vector, 2);
+  slip_mask = abs(tangent_stress) > NO_SLIP_FORCE;
+  slip_mask(1) = 0;
+  slip_mask(end) = 0;
+  if ~any(slip_mask)
+    break;
   end
+  slip_iter = slip_iter + 1;
+  mid_point = (new_X2(kp, :) + new_X2(km, :)) ./ 2;
+  correction = dot(mid_point - new_X2, unit_tangent_vector, 2) .* unit_tangent_vector;
+  new_X2 = new_X2 + correction .* slip_mask;
+  tmpF = WALL_STIFFNESS * (new_X2(kp, :) + new_X2(km, :) - 2*new_X2) / dtheta2;
+  plot(new_X2(slip_mask, 1), new_X2(slip_mask, 2), 'ko');
+end
+if slip_iter > 2
+  fprintf("slip iter %d \n", slip_iter);
 end
