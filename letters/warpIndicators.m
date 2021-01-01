c7cb3b280935aa7d1de5ccd216e32943be9d1d68
Daniel Chin <daniel_chin@yahoo.com>
2021-01-01 17:14:25 +0800

    warp X4

--- letters/warpIndicators.m
+++ letters/warpIndicators.m
@@ -9,3 +9,15 @@ for k = 1 : 2
     end
   end
 end
+for k = 1 : 2
+  for j = 1 : Nb4
+    if X4(j, k) < 0
+      X4(j, k) = X4(j, k) + L;
+      Y4(j, k) = Y4(j, k) + L;
+    end
+    if X4(j, k) > L
+      X4(j, k) = X4(j, k) - L;
+      Y4(j, k) = Y4(j, k) - L;
+    end
+  end
+end
