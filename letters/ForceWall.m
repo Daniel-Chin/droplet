12241474aebfc4cbaa4773e218b925f2d2bddf10
Daniel Chin <daniel_chin@yahoo.com>
2021-01-01 17:16:01 +0800

    slip_iter capped

--- letters/ForceWall.m
+++ letters/ForceWall.m
@@ -14,7 +14,7 @@ while 1
   slip_mask = abs(tangent_stress) > NO_SLIP_FORCE;
   slip_mask(1) = 0;
   slip_mask(end) = 0;
-  if ~any(slip_mask)
+  if ~any(slip_mask) || slip_iter > 12
     break;
   end
   slip_iter = slip_iter + 1;
@@ -22,8 +22,8 @@ while 1
   correction = dot(mid_point - new_X2, unit_tangent_vector, 2) .* unit_tangent_vector;
   new_X2 = new_X2 + correction .* slip_mask;
   tmpF = WALL_STIFFNESS * (new_X2(kp, :) + new_X2(km, :) - 2*new_X2) / dtheta2;
-  plot(new_X2(slip_mask, 1), new_X2(slip_mask, 2), 'ko');
+  % plot(new_X2(slip_mask, 1), new_X2(slip_mask, 2), 'ko');
 end
-if slip_iter >= 10
-  fprintf("large slip_iter, %d \n", slip_iter);
-end
+% if slip_iter >= 10
+%   fprintf("large slip_iter, %d \n", slip_iter);
+% end
