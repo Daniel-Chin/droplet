11e7cdb1cb519d766e8498e96d8d1567c0fb81ab
Daniel Chin <daniel_chin@yahoo.com>
2021-01-13 11:10:34 +0800

    more presets framerate

--- setFrameRate.m
+++ setFrameRate.m
@@ -1,7 +1,8 @@
 % FPS = 1 / dt;
 % FPS = 60 * 80;
-FPS = 60 * 40;
+% FPS = 60 * 40;
 % FPS = 60 * 20;
+FPS = 60 * 5;
 % FPS = 60 * 1;
 SPF = 1 / FPS;
 approx_timesteps_per_frame = floor(1 / dt / FPS / 2) * 2 + 1;
