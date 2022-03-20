cff05de81ae8990cc64baf5793a7886f004ec487
Daniel Chin <daniel_chin@yahoo.com>
2022-03-19 22:27:24 -0400

    constants

--- equiDefineConstants.m
+++ equiDefineConstants.m
@@ -10,8 +10,8 @@ rho = .1; % air density g/cm2
 rho_heavy = 1; % density g/cm2
 mu = 1; % viscosity g/s. 2D water can be 0.00089
 tmax = .25; % Run until time s
-NO_SLIP_FORCE = 2500;
-FRICTION_ADJUST = 30;
+NO_SLIP_FORCE = 100;
+FRICTION_ADJUST = .1;
 WALL_SPACING = .5;
 STATIC_CONTACT_ANGLE = pi / 2;
 
