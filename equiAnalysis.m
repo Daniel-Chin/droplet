fce16c4da7000c2ad136125c051eaa31cae42bb1
Daniel Chin <daniel.chin@nyu.edu>
2022-03-25 22:43:10 -0400

    new equi analy

--- equiAnalysis.m
+++ equiAnalysis.m
@@ -16,16 +16,25 @@ while clock < clockmax
   clock = clock + 1;
   step();
   if mod(clock, 16) == 0
-    curavture_estimation_radius = 2;
+    curavture_estimation_radius = round(N / 24);
+  % curavture_estimation_radius = 2;
     equiDoIt_normalized();
     alt = curvature_pairs(3:end-2, 1);
     cvt = - curvature_pairs(3:end-2, 2);
     fit_result = polyfit(cvt, alt, 1);
-    slope_err = fit_result(1) - (- K / big_G);
+    slope_err = fit_result(1) - (- K / 1000);
     slope_err_his_i = slope_err_his_i + 1;
     slope_err_his(1, slope_err_his_i) = clock * dt;
     slope_err_his(2, slope_err_his_i) = slope_err;
   end
+  % if mod(clock, 64) == 0
+  %   hold off;
+  %   simDots = plot(alt, cvt);
+  %   axis([.6 1.6 -20 20]);
+  %   drawnow;
+  %   title(sprintf('t = %.3f', clock * dt));
+  %   saveFrame();
+  % end
 
   if schedule_next_frame_pause
     pause;
@@ -37,5 +46,6 @@ save(sprintf('results/%s/%d_%d_%f.mat', exp_name, big_G, N, dt));
 % plot(slope_err_his(1, :), slope_err_his(2, :));
 % hold on;
 % line(xlim(), [0 0], 'Color', 'k');
+% xlim([slope_err_his(1, round(end * .05 / tmax)), slope_err_his(1, end)]);
 % hold off;
 % pause;
