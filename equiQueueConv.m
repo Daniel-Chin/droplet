5e0e0375879a0f8ea6075d3d17017425a257eb13
Daniel Chin <daniel.chin@nyu.edu>
2022-03-26 19:38:44 -0400

    new queue

--- equiQueueConv.m
+++ equiQueueConv.m
@@ -9,23 +9,29 @@ exp_name = "equilibrium_conv";
 
 big_G = 1000; % cm/s2
 dt = 0.0004; % Time step, second
-dt = 0.0001; % Time step, second
+dt = 0.00005; % Time step, second
 N = 24; % Number of grid cells
 equiAnalysis;
 
 big_G = 1000; % cm/s2
 dt = 0.0002; % Time step, second
-dt = 0.0001; % Time step, second
-N = 32; % Number of grid cells
-equiAnalysis;
-
-big_G = 1000; % cm/s2
-dt = 0.0001; % Time step, second
-dt = 0.0001; % Time step, second
+dt = 0.00005; % Time step, second
 N = 48; % Number of grid cells
 equiAnalysis;
 
 big_G = 1000; % cm/s2
 dt = 0.0001; % Time step, second
+dt = 0.00005; % Time step, second
+N = 72; % Number of grid cells
+equiAnalysis;
+
+big_G = 1000; % cm/s2
+dt = 0.0001; % Time step, second
+dt = 0.00005; % Time step, second
 N = 96; % Number of grid cells
+equiAnalysis;
+
+big_G = 1000; % cm/s2
+dt = 0.00005; % Time step, second
+N = 192; % Number of grid cells
 equiAnalysis;
